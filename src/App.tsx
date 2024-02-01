733428b4144842066014ccba80f7560886d0f7d2
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2024-02-01 10:54:26 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='text-red-600'>
+      <h1 className=''>Hello, wold</h1>
+      <h1 className=''>Hello, wold</h1>
+      <h1 className=''>Hello, wold</h1>
+      <h1 className=''>Hello, wold</h1>
+    </div>
+  )
+}
+
+export default App
